f9275d349129658778a5beab8d35b6e20666b3e6
Sergey <sergeyyakovlev99@gmail.com>
2023-02-10 14:55:46 +0300

    I add segmentTree code

---
+++ Segment Tree SetSummCompressedRequests/Segment Tree SetSummCompressedRequests/Segment Tree SetSummCompressedRequests.cpp
@@ -0,0 +1,9 @@
+﻿// Segment Tree SetSummCompressedRequests.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
