07ebd5006b50678c9c2f2dd2f7c11db9ef90042d
Sergey <sergeyyakovlev99@gmail.com>
2023-02-13 15:17:57 +0300

    13.02.2023

--- Segment Tree SetSummCompressedRequests/Segment Tree SetSummCompressedRequests/Segment Tree SetSummCompressedRequests.cpp
+++ Segment Tree SetSummCompressedRequests/Segment Tree SetSummCompressedRequests/Segment Tree SetSummCompressedRequests.cpp
@@ -1,9 +1,46 @@
-﻿// Segment Tree SetSummCompressedRequests.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include<vector>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<set>
 
-#include <iostream>
+using namespace std;
+#define ll long long
+#define int long long
 
-int main()
+struct Node
 {
-    std::cout << "Hello World!\n";
+    Node* left = NULL, * right = NULL;
+    int value = 0, push = 0, cntOfZeros = 0;
+    Node() { value = 0; }
+
+    Node(int value, int cntOfZeros)
+    {
+        this->value = value;
+        this->cntOfZeros = cntOfZeros;
+    }
+
+    Node(Node* left, Node* right)
+    {
+        this->value = left->value + right->value;
+        this->cntOfZeros = left->cntOfZeros + right->cntOfZeros;
+        this->left = left;
+        this->right = right;
+    }
+};
+
+Node* build(int n)
+{
+    return new Node(0, n);
 }
+
+
+
+signed main() {
+    int n;
+    cin >> n;
+    SegmentTree st = {n};
+    cout << st.st->cntOfZeros;
+    
+}
+
